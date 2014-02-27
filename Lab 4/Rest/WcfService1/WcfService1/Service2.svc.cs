67a4928baa7ea3b79ab24845360fe55e07be6dfc
sshy3 <sshy3@mail.umkc.edu>
2014-02-26 23:57:48 -0600

    done

---
+++ Lab 4/Rest/WcfService1/WcfService1/Service2.svc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging
+    public class Service2 : IService2
+    {
+        [WebInvoke(Method = "GET",ResponseFormat = WebMessageFormat.Json,UriTemplate = "convtob/{id}")]
+        public calce calculate (string id)
+        {
+            return new calce()
+            {
+                
+                cel= convertob(id)
+
+            };
+         }
+            public string convertob(string id )
+            {   int f = Convert.ToInt32(id);
+                string binary = Convert.ToString(f, 2);
+                return binary;
+            }
+
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+      
+    }
+
+    public class calce
+    {
+        public string cel { get; set; }
+    }  
+}
+
